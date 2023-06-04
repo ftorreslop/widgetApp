ef2511f2d3db6644e31fd1c946f00ac62c92387e
ftorreslop <ftorreslop@gmail.com>
2023-06-04 10:28:04 +0000

    Routes with name

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:widgetsapp/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home';
+
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
